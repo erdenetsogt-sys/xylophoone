651330fb16f736e799e0435383660c98841d6cd5
erdenetsogt-sys <tsogt1225@gmail.com>
2023-08-10 17:42:32 +0900

    xylophone

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+    @IBAction func buttonC(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        print("started")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+           // Code you want to be delayed
+            print("end")
+            sender.alpha = 1.0
+        }
+    }
+    
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+    
+    
+
+    
+
+}
+
